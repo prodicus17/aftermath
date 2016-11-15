f479c9c7dbff469b7721847b411dfd9dd8708a8a
sanderson <sanderson@SEMEXHQ-DT007.semex.com>
2016-11-15 15:45:30 -0500

    initial push

---
+++ src/ca/elliottanderson/aftermath/Launcher.java
@@ -0,0 +1,13 @@
+package ca.elliottanderson.aftermath;
+
+import ca.elliottanderson.aftermath.display.Display;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Display("Aftermath", 300, 300 );
+
+	}
+
+}
